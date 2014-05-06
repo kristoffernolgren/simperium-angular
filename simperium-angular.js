b55562f5783ad54a283bf7bffbfc0eeeaaf1bb19
Kristoffer Nolgren <kirstoffer@nolgren.se>
2014-05-06 17:00:18 +0200

    .

---
+++ simperium-angular.js
@@ -0,0 +1,8 @@
+angular.module('simperiumAngular', []).
+config(function(){
+	console.log(simperiumCred)
+//	var simperium = new Simperium('SIMPERIUM_APP_ID', { token : 'SIMPERIUM_ACCESS_TOKEN'});
+}).
+service('auth', function(){
+
+});
